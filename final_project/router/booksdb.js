ea114bff185910430e591713f4714e80c5544568
SandeshNepali <112896956+SandeshNepali@users.noreply.github.com>
2024-08-29 15:56:58 +0545

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,15 +1,14 @@
-// books.js
 const books = [
-    { isbn: '1', author: "Chinua Achebe", title: "Things Fall Apart", reviews: {} },
-    { isbn: '2', author: "Hans Christian Andersen", title: "Fairy tales", reviews: {} },
-    { isbn: '3', author: "Dante Alighieri", title: "The Divine Comedy", reviews: {} },
-    { isbn: '4', author: "Unknown", title: "The Epic Of Gilgamesh", reviews: {} },
-    { isbn: '5', author: "Unknown", title: "The Book Of Job", reviews: {} },
-    { isbn: '6', author: "Unknown", title: "One Thousand and One Nights", reviews: {} },
-    { isbn: '7', author: "Unknown", title: "Njál's Saga", reviews: {} },
-    { isbn: '8', author: "Jane Austen", title: "Pride and Prejudice", reviews: {} },
-    { isbn: '9', author: "Honoré de Balzac", title: "Le Père Goriot", reviews: {} },
-    { isbn: '10', author: "Samuel Beckett", title: "Molloy, Malone Dies, The Unnamable, the trilogy", reviews: {} }
+      { isbn: '1', author: "Chinua Achebe", title: "Things Fall Apart", reviews: ["Sandesh Nepali"] },
+      { isbn: '2', author: "Hans Christian Andersen", title: "Fairy tales", reviews: ["Jamie"] },
+      { isbn: '3', author: "Dante Alighieri", title: "The Divine Comedy", reviews: ["Morgan"] },
+      { isbn: '4', author: "Unknown", title: "The Epic Of Gilgamesh", reviews: ["Taylor"] },
+      { isbn: '5', author: "Unknown", title: "The Book Of Job", reviews: ["Jordan"] },
+      { isbn: '6', author: "Unknown", title: "One Thousand and One Nights", reviews: ["Casey"] },
+      { isbn: '7', author: "Unknown", title: "Njál's Saga", reviews: ["Riley"] },
+      { isbn: '8', author: "Jane Austen", title: "Pride and Prejudice", reviews: ["Parker"] },
+      { isbn: '9', author: "Honoré de Balzac", title: "Le Père Goriot", reviews: ["Avery"] },
+      { isbn: '10', author: "Samuel Beckett", title: "Molloy, Malone Dies, The Unnamable, the trilogy", reviews: ["Quinn"] }
 ];
 
 module.exports = books;
